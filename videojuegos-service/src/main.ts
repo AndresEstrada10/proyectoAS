5f809f357cc01b12c7af4ce01468d8b9cc0b3df4
AndresEstrada10 <e1351924046@live.uleam.edu.ec>
2024-12-03 13:16:51 -0500

    configuracion puerto dinaminico

--- videojuegos-service/src/main.ts
+++ videojuegos-service/src/main.ts
@@ -4,13 +4,22 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const isProduction = process.env.NODE_ENV === 'production';
+
   // Habilitar CORS para permitir solicitudes desde el frontend
   app.enableCors({
-    origin: 'http://localhost:3001', // Dirección del frontend
+    origin: isProduction
+      ? 'https://proyectoas-2.onrender.com' // Cambia al dominio de tu frontend en producción
+      : 'http://localhost:3001', // Cambia al puerto donde corre tu frontend localmente
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  await app.listen(3000);
+  // Usar el puerto proporcionado por Render o el puerto 3000 por defecto
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(
+    `Application is running on: ${isProduction ? 'https://proyectoas-2.onrender.com' : `http://localhost:${port}`}`
+  );
 }
 bootstrap();
